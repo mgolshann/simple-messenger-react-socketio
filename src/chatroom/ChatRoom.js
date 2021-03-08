9c89cded05d7378e2d8bcced993c3c90a561e920
mgolshann <m_golshan85@yahoo.com>
2021-03-07 22:40:25 -0800

    create user messages

--- src/chatroom/ChatRoom.js
+++ src/chatroom/ChatRoom.js
@@ -1,20 +1,77 @@
 import { Grid, IconButton, InputBase, Paper, Typography } from '@material-ui/core';
-import React from 'react';
+import React, { useRef } from 'react';
 import io from 'socket.io-client';
 import useStyle from './style';
 import SendIcon from '@material-ui/icons/Send';
+import classNames from 'classnames';
 
 const ChatRoom = (props) => {
+    // const messages = [
+    //     {
+    //         id: "",
+    //         msg: "Hi it's me Tom",
+    //         sender: {
+    //             name: "tom",
+    //             gender: 0
+    //         }
+    //     }, {
+    //         id: "",
+    //         msg: "hi tom how you doing ?",
+    //         sender: {
+    //             name: "elli",
+    //             gender: 1
+    //         }
+    //     }, {
+    //         id: "",
+    //         msg: "thanks, and you ??",
+    //         sender: {
+    //             name: "tom",
+    //             gender: 0
+    //         }
+    //     }, {
+    //         id: "",
+    //         msg: "I am ok thanks",
+    //         sender: {
+    //             name: "elli",
+    //             gender: 1
+    //         }
+    //     },
+    // ];
 
+    const scrollableGrid = useRef();
     const classes = useStyle();
+    //const socket = React.useRef(io.connect("http://localhost:3010/socket"));
     const socket = React.useRef(io('http://localhost:3010/socket', { transports: ['websocket', 'polling', 'flashsocket'] }));
 
-    const [newMessage, setnewMessage] = React.useState("");
-    //const socket = React.useRef(io.connect("http://localhost:3010/socket"));
+    const [messages, setMessages] = React.useState([]);
+    const [newMessage, setnewMessage] = React.useState([]);
+
+    const sendMessage = () => {
+        if (!newMessage) return;
+        socket.current.emit("messageFromClientToServer", {
+            id: "10",
+            msg: newMessage,
+            sender: {
+                name: props.location.state.name,
+                gender: props.location.state.gender
+            }
+        });
+        setnewMessage("");
+    };
+
+    const _handleInput = (e) => {
+        if (e.key === "Enter") {
+            sendMessage();
+        }
+    }
 
     React.useEffect(() => {
         //console.log(props.location.state)
-    })
+        socket.current.on("messageFromServerToClient", (msg) => {
+            setMessages(messages => messages.concat(msg));
+            scrollableGrid.current.scroll(0, scrollableGrid.current.scrollHeight);
+        })
+    }, [])
 
     return (
         <Paper className={classes.paper}>
@@ -22,16 +79,42 @@ const ChatRoom = (props) => {
                 <Grid item className={classes.header} container alignItems={"center"} justify={"center"}>
                     <Typography className={classes.headerText}>Chat Room</Typography>
                 </Grid>
-                <Grid item className={classes.middle}>
-                    
+                <Grid item className={classes.middle} ref={scrollableGrid}>
+                    {
+                        messages.map((message) =>
+                            <Grid keys={message.id} item container className={classNames(classes.messageParent,
+                                message.sender.gender !== props.location.state.gender && classes.message_reverse)}
+                                alignItems={"center"}>
+                                <Grid item>
+                                    <img title={message.sender.name} src={message.sender.gender === 0 ? "/images/male-avatar.png" : "/images/woman-avatar.png"}
+                                        alt={message.sender.name} className={classes.avatar} />
+                                </Grid>
+                                <Grid item>
+                                    <div className={classNames(classes.messageBorder,
+                                        message.sender.name !== props.location.state.name ? classes.messageMe : classes.messageHe)}>
+                                        <Typography className={classes.sender}>
+                                            {message.sender.name}
+                                        </Typography>
+                                        <Typography className={classes.msg}>
+                                            {message.msg}
+                                        </Typography>
+                                        <Typography className={classes.date}>
+                                            {message.date.split('T')[1].split('.')[0]}
+                                        </Typography>
+                                    </div>
+                                </Grid>
+                            </Grid>
+                        )
+                    }
+
                 </Grid>
                 <Grid direction={"row-reverse"} item className={classes.footer} container justify={"center"} alignItems={"center"}>
                     <Grid item xs>
-                        <InputBase className={classes.input}
-                            value={newMessage} onChange={e => setnewMessage(e.target.value)}  />
-                    </Grid> 
+                        <InputBase className={classes.input} onKeyDown={_handleInput}
+                            value={newMessage} onChange={e => setnewMessage(e.target.value)} />
+                    </Grid>
                     <Grid item>
-                        <IconButton className={classes.btnSend}>
+                        <IconButton onClick={sendMessage} className={classes.btnSend}>
                             <SendIcon />
                         </IconButton>
                     </Grid>
--- src/chatroom/style.js
+++ src/chatroom/style.js
@@ -6,6 +6,7 @@ const useStyle = makeStyles(theme => ({
         width: "50rem",
         height: "max-content",
         margin: "3rem auto",
+        scrollBehavior: "smooth"
     },
     header: {
         background: "#DDE",
@@ -17,7 +18,9 @@ const useStyle = makeStyles(theme => ({
     },
     middle: {
         background: "white",
-        height: "20rem"
+        height: "20rem",
+        overflowY: "auto",
+        
     },
     footer: {
         background: "#DDE",
@@ -34,8 +37,84 @@ const useStyle = makeStyles(theme => ({
         textAlign: "left",
         direction: "ltr"
     },
+    avatar: {
+        width: "4rem",
+        borderRadius: "50%",
+        marginLeft: "1rem"
+    },
     btnSend: {
         margin: "1rem"
+    },
+    messageParent: {
+        marginTop: "1rem",
+        alignItems: "center"
+    },
+    message_reverse: {
+        flexDirection: "row-reverse"
+    },
+    messageBorder: {
+        border: "1px solid #ececec",
+        borderRadius: "2rem",
+        backgroundColor: "#ebebebe",
+        padding: "1rem 1.5rem"
+    },
+    msg: {
+        direction: "ltr"
+    },
+    sender: {
+        color: "#aaa",
+        fontSize: "0.8rem",
+        marginBottom: "1rem"
+    },
+    date: {
+        color: "#aaa",
+        fontSize: "0.8rem",
+        marginTop: "1rem"
+    },
+    messageMe: {
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "flex-start",
+        marginLeft: "1rem",
+        position: "relative",
+        background: "#ebebeb",
+        borderRadius: ".4em",
+        "&:after": {
+            content: "''",
+            position: "absolute",
+            left: "0",
+            top: "50%",
+            width: "0",
+            height: "0",
+            border: "20px solid transparent",
+            borderRightColor: "#ebebeb",
+            borderLeft: "0",
+            borderBottom: "0",
+            marginTop: "-10px",
+            marginLeft: "-20px"
+        }
+    },
+    messageHe: {
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "flex-end",
+        position: "relative",
+        background: "#ebebeb",
+        borderRadius: ".4em",
+        "&:after": {
+            content: "''",
+            position: "absolute",
+            right: "0",
+            top: "50%",
+            width: "0",
+            height: "0",
+            border: "20px solid transparent",
+            borderLeftColor: "#ebebeb",
+            borderRight: "0",
+            borderBottom: "0",
+            marginTop: "-10px",
+            marginRight: "-20px"
+        }
     }
 }));
 
